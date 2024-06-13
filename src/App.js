1cc695f7a31538f8418c7862f1a17ec8a7ccca59
Anna <99702604+Anna-R0@users.noreply.github.com>
2024-06-12 22:56:46 -0700

    Added BS
    Added BS

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
+
+      
      <h1>Weather App </h1>
      <footer>
       This project was coded by Anna Ro and is <a href= "https://github.com/Anna-R0/weather-app2024" target="_blank">open-sourced on GitHub</a>
      </footer>
+    </div>
     </div>
   );
 }
