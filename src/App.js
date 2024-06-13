f26ccecefcc4d33048ecbe9b2a3b123142edc700
Anna <99702604+Anna-R0@users.noreply.github.com>
2024-06-12 22:52:17 -0700

    Update App.js
    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App </h1>
+     <footer>
+      This project was coded by Anna Ro and is <a href= "https://github.com/Anna-R0/weather-app2024" target="_blank">open-sourced on GitHub</a>
+     </footer>
     </div>
   );
 }
